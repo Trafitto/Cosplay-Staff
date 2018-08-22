a395930656bc34347c603e7b23efc2eb73293776
borchimarco <borchimarco@hotmail.it>
2018-08-22 15:23:23 +0200

    Fixing strange compiling error deleting some line ¯\_(ツ)_/¯

--- CosplayStaff.ino
+++ CosplayStaff.ino
@@ -1,8 +1,7 @@
-//#include <Adafruit_NeoPixel.h>
+
 
 
 #include "Util.h"
-#include <Wire.h>
 
     #define D0 16
     #define D1 5 //USE THIS
@@ -17,38 +16,19 @@
 
 //Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, D1, NEO_GRB + NEO_KHZ800);
 
-const int MPU=0x68;
-int GyX,GyY,GyZ;
 
 void setup() {
   init(1,D1);
   setAllOff();
-  // Initialize MPU-6050
- /* Wire.begin();
-  Wire.beginTransmission(MPU);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);8/
+
 }
 
 void loop() {
 
-  /*Wire.beginTransmission(MPU);
-  Wire.write(0x3B);  
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU,14,true);
-  GyX=Wire.read()<<8|Wire.read();  
-  GyY=Wire.read()<<8|Wire.read(); 
-   GyZ=Wire.read()<<8|Wire.read();
-  Serial.print("Gyroscope: ");
-  Serial.print("X = "); Serial.print(GyX);
-  Serial.print(" | Y = "); Serial.print(GyY);
-  Serial.print(" | Z = "); Serial.println(GyZ);
-  Serial.println(" ");
-  delay(333);*/
+
   //ShowExample();
 
-Breath(1,255,0.02,0.01);
+  Breath(1,255,0.02,0.01);
 }
 
 
--- Util.h
+++ Util.h
@@ -1,19 +1,20 @@
 #include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
+#include <Wire.h>
 Adafruit_NeoPixel strip;
 
 
 void init(int nLed,int pin)
 {
 	
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+    // Parameter 1 = number of pixels in strip
+    // Parameter 2 = Arduino pin number (most are valid)
+    // Parameter 3 = pixel type flags, add together as needed:
+    //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+    //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+    //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+    //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+    //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 	strip=Adafruit_NeoPixel(nLed, pin, NEO_GRB + NEO_KHZ800);
 }
 
